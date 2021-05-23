4f8677ba59160040928b4c8307cfa38f3c35f9b3
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-05-23 17:47:27 -0400

    added login route with error handling

--- functions/index.js
+++ functions/index.js
@@ -108,7 +108,7 @@ app.post("/signup", (req, res) => {
     if (isEmpty(newUser.email)) {
         errors.email = "Must not be blank.";
     } else if (!isEmail(newUser.email)) {
-        errors.email = "Must be a valid email address.";
+        errors.email = "Invalid Email Address.";
     }
 
     // Password Validation
@@ -160,6 +160,43 @@ app.post("/signup", (req, res) => {
             console.error(err);
             if (err.code === "auth/email-already-in-use") {
                 return res.status(400).json({ email: "Email already in use." });
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        });
+});
+
+// @desc Login user
+// @route POST /login
+// @access Public
+app.post("/login", (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = "Must not be empty.";
+    if (isEmpty(user.password)) errors.password = "Must not be empty.";
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code === "auth/wrong-password") {
+                return res
+                    .status(403)
+                    .json({ general: "Wrong credentials, please try again." });
             } else {
                 return res.status(500).json({ error: err.code });
             }
