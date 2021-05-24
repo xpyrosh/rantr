11b83c7bcb11dde0644031441bb10871ac892788
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-05-23 20:08:57 -0400

    refactored new post route to include middleware for authorizing the user token and then connecting the new post to the logged in user

--- functions/index.js
+++ functions/index.js
@@ -54,13 +54,48 @@ app.get("/posts", (req, res) => {
         .catch((err) => console.error(err));
 });
 
+// Authenticating user token
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer ")
+    ) {
+        idToken = req.headers.authorization.split("Bearer ")[1];
+    } else {
+        console.error("No token found.");
+        return res.status(403).json({ error: "Unauthorized." });
+    }
+
+    admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db
+                .collection("users")
+                .where("userId", "==", req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then((data) => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch((err) => {
+            console.error("Error verifying token.", err);
+            res.status(403).json(err);
+        });
+};
+
 // @desc Create new Post
 // @route POST /posts
 // @access Public
-app.post("/posts", (req, res) => {
+app.post("/posts", FBAuth, (req, res) => {
     const newPost = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString(),
     };
 
